55ecea6c19d41e1e5696c93b5331933be0d660db
abdu102 <abduvohidovabdulvoris@gmail.com>
2025-08-10 12:34:47 +0500

    Routing: switch to HashRouter to prevent route reload 404s across hosts

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastProvider } from './components/Toast';
 import { LanguageProvider } from './components/Language';
